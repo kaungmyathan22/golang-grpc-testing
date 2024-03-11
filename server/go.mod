3ea18989d25f742afa5592f054ed36f7d87ade0d
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-03-11 23:35:53 +0630

    feat(): json rpc srevr added

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,8 @@
 module github.com/kaungmyathan22/golang-grpc-server
 
 go 1.21.5
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/gorilla/rpc v1.2.1 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -1,32 +1,67 @@
 package main
 
 import (
+	jsonparse "encoding/json"
+	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
-	"net/rpc"
-	"time"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/rpc"
+	"github.com/gorilla/rpc/json"
 )
 
-type Args struct{}
-type TimeServer int64
+// Args holds arguments passed to JSON RPC service
+type Args struct {
+	Id string
+}
 
-func (t *TimeServer) GiveServerTime(args *Args, reply *int64) error {
-	// Fill reply pointer to send the data back
-	*reply = time.Now().Unix()
+// Book struct holds Book JSON structure
+type Book struct {
+	Id     string `"json:string,omitempty"`
+	Name   string `"json:name,omitempty"`
+	Author string `"json:author,omitempty"`
+}
+type JSONServer struct{}
+
+// GiveBookDetail
+func (t *JSONServer) GiveBookDetail(r *http.Request, args *Args, reply *Book) error {
+	var books []Book
+	// Read JSON file and load data
+	raw, readerr := ioutil.ReadFile("./books.json")
+	if readerr != nil {
+		log.Println("error:", readerr)
+
+		os.Exit(1)
+	}
+
+	// Unmarshal JSON raw data into books array
+	marshalerr := jsonparse.Unmarshal(raw, &books)
+	if marshalerr != nil {
+		log.Println("error:", marshalerr)
+
+		os.Exit(1)
+	}
+
+	// Iterate over each book to find the given book
+	for _, book := range books {
+		if book.Id == args.Id {
+			// If book found, fill reply with it
+			*reply = book
+			break
+		}
+	}
+
 	return nil
 }
 func main() {
 	// Create a new RPC server
-	timeserver := new(TimeServer)
-	// Register RPC server
-	rpc.Register(timeserver)
-	rpc.HandleHTTP()
-	// Listen for requests on port 1234
-	l, e := net.Listen("tcp", ":1234")
-	if e != nil {
-		log.Fatal("listen error:", e)
-	}
-	log.Println("server is running.....")
-	http.Serve(l, nil)
+	s := rpc.NewServer() // Register the type of data requested as JSON
+	s.RegisterCodec(json.NewCodec(), "application/json")
+	// Register the service by creating a new JSON server
+	s.RegisterService(new(JSONServer), "")
+	r := mux.NewRouter()
+	r.Handle("/rpc", s)
+	http.ListenAndServe(":1234", r)
 }
